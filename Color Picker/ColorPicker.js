71c7ff185131efce1e1b71beb260e481c02e310f
Loki-24 <lokesh24lanka@gmail.com>
2023-01-20 15:06:24 +0530

    Background color will change accordingly

---
+++ Color Picker/ColorPicker.js
@@ -0,0 +1,22 @@
+let whole = document.getElementById("colorPickerContainer");
+let spanText = document.getElementById("selectedColorHexCode");
+
+function ChangeBg1() {
+    whole.style.backgroundColor = "#e0e0e0";
+    spanText.textContent = "#e0e0e0";
+}
+
+function ChangeBg2() {
+    whole.style.backgroundColor = "#6fcf97";
+    spanText.textContent = "#6fcf97";
+}
+
+function ChangeBg3() {
+    whole.style.backgroundColor = "#56ccf2";
+    spanText.textContent = "#56ccf2";
+}
+
+function ChangeBg4() {
+    whole.style.backgroundColor = "#bb6bd9";
+    spanText.textContent = "#bb6bd9";
+}
