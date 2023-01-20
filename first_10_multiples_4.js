ad6703597988e75d47abe4d7f8fad30970200391
Loki-24 <lokesh24lanka@gmail.com>
2023-01-20 14:54:28 +0530

    Multiples of 4

---
+++ first_10_multiples_4.js
@@ -0,0 +1,21 @@
+
+//using for loop//
+for(i=1;i<=40;1+=4){
+    console.log(i)
+}
+
+//using while loop//
+
+i=1
+while (i<=40){
+    console.log(i);
+    i+=4
+}
+
+//using Do-while loop//
+
+i=1
+do{
+    console.log(i)
+    i+=4
+} while(i<=40);
