4ea00846f8edae30b381c3ecb4990f69a6980724
Loki-24 <lokesh24lanka@gmail.com>
2023-01-20 15:01:06 +0530

    even nums < 10

---
+++ first_even_num.js
@@ -0,0 +1,21 @@
+
+//using for loop//
+for(i=0;i<=10;i+=2){
+    console.log(i)
+}
+
+//using while loop//
+
+i=0
+while (i<=10){
+    console.log(i);
+    i=i+2;
+}
+
+//using Do-while loop//
+
+i=0
+do{
+    console.log(i)
+    i=i+2
+} while(i<=10);
