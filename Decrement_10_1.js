eb71f03158636350159f38c77c4721fd564771a0
Loki-24 <lokesh24lanka@gmail.com>
2023-01-20 14:50:45 +0530

    10 to 1

---
+++ Decrement_10_1.js
@@ -0,0 +1,18 @@
+//using for loop//
+for(i=10;i>=1;i-=1){
+    console.log(i)
+}
+
+//using while loop//
+i=10
+while(i>=1){
+    console.log(i)
+    i-=1
+}
+
+// using Do-While loop//
+i=10
+do{
+    console.log(i)
+    i-=1
+}while(i>=1)
