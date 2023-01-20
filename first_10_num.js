2549be94fd3f10d5bb73a8ce9c8571f24fe7b47b
Loki-24 <lokesh24lanka@gmail.com>
2023-01-20 14:55:47 +0530

    1 to 10

---
+++ first_10_num.js
@@ -0,0 +1,19 @@
+
+//using for loop//
+for(i=1;i<=10;i+=1){
+    console.log(i)
+}
+
+//using while loop//
+var i=1
+while(i<=10){
+    console.log(i)
+    i+=1
+}
+
+// using Do-While loop//
+var i=1
+do{
+    console.log(i)
+    i+=1
+} while(i<=10);
