ad642d0c8b5461395f2c8905644741d39ded66c9
Loki-24 <lokesh24lanka@gmail.com>
2023-01-20 15:04:40 +0530

    odd nums < 10

---
+++ first_odd_num.js
@@ -0,0 +1,20 @@
+//using for loop//
+for(i=1;i<=10;i+=2){
+    console.log(i)
+}
+
+//using while loop//
+
+i=1
+while (i<=10){
+    console.log(i);
+    i=i+2;
+}
+
+//using Do-while loop//
+
+i=1
+do{
+    console.log(i)
+    i=i+2
+} while(i<=10);
