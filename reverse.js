d6b923ec2aec5ab420d33f02a678bb7890b653ce
Loki-24 <lokesh24lanka@gmail.com>
2023-01-23 15:31:34 +0530

    reversing the elements in it

---
+++ reverse.js
@@ -0,0 +1,27 @@
+// using reverse command
+let a = [10, 20, 30, 40, "REVI"]
+a.reverse()
+console.log(a)
+
+// using for loop
+let b = [10, 20, 30, 40, "REVI"]
+for (i=4;i>=0;i--)
+{
+    console.log(b[i])
+}
+
+// using while loop
+let c = [10, 20, 30, 40, "REVI"]
+var i =4
+while(i>=0){
+    console.log(c[i])
+    i-=1
+}
+
+// using Do-while loop
+let d = [10, 20, 30, 40, "REVI"]
+var i=4
+do{
+    console.log(d[i])
+    i--
+}while (i>=0);
