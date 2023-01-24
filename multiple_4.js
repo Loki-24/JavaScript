901fdad1d65342561e5bdbdf4bec59a6146dccdf
Loki-24 <lokesh24lanka@gmail.com>
2023-01-24 15:06:22 +0530

    multiples of 4 using increment

---
+++ multiple_4.js
@@ -0,0 +1,16 @@
+// printing first 10 nums of 4 multiples using for loop
+for(i=0;i<=40;i=i+4){
+    console.log(i)
+}
+// printing first 10 nums of 4 multiples using while loop
+var i = 0
+while (i<=40){
+    console.log(i)
+    i+=4
+}
+// printing first 10 nums of 4 multiples using Do-While loop
+var i=0
+do{
+    console.log(i)
+    i+=4
+} while(i<=40);
